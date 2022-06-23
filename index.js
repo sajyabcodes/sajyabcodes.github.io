887fd51c75c1297d6f3d4b904f4ae5c0786c7b70
sajyabcodes <106924169+sajyabcodes@users.noreply.github.com>
2022-06-24 00:33:31 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,47 @@
+
+//scroll top
+let mybutton = document.getElementById("myBtn");
+
+window.onscroll = function(){
+  scrollFunction()
+};
+
+function scrollFunction(){
+  if (document.body.scrollTop > 20 ||
+    document.documentElement.scrollTop > 20){
+    mybutton.style.display = "block";
+  } else {
+     mybutton.style.display = "none";
+ }
+}
+//videos
+function topFunction(){
+ document.body.scrollTop = 0;
+ document.documentElement.scrollTop = 0;
+}
+
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
+let listVideo = document.querySelectorAll(".video-list .vid");
+let mainVideo = document.querySelector(".main-video video");
+let title = document.querySelector(".main-video .title");
+
+listVideo.forEach(video =>{
+  video.onclick = () =>{
+    listVideo.forEach(vid => vid.classList.remove("active"));
+    video.classList.add("active");
+    if(video.classList.contains("active")){
+      let src = video.children[0].getAttribute("src");
+      mainVideo.src = src;
+      let text = video.children[1].innerHTML;
+      title.innerHTML = text;
+  };
+ };
+});
